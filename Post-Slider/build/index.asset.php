ab0145fc8e312f7524593e499d1db3cbde6d7e34
PranitDugad <pranit.dugad@gmail.com>
2025-03-01 15:08:10 +0530

    Added Final Pugin for assesment

--- Post-Slider/build/index.asset.php
+++ Post-Slider/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-server-side-render'), 'version' => 'f96f27d8d479fad91246');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-server-side-render'), 'version' => '6d6618cc75431c631395');
